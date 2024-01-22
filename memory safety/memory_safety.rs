4039f0ad4c6a203545911bf9c3897874248cae0d
ajitsoni007 <ajit.soni@amagi.com>
2024-01-22 23:14:29 +0530

    memory safety module completed

---
+++ memory safety/memory_safety.rs
@@ -0,0 +1,40 @@
+fn main(){
+
+
+  /*
+    let enigma: i32;
+
+    println!("{}",enigma);
+
+    
+    It is not allowed to use or capture an uninitialized variable.
+
+    Erroneous code example:
+
+    fn main() {
+        let x: i32;
+        let y = x; // error, use of possibly-uninitialized variable
+    }
+
+    To fix this, ensure that any declared variables are initialized before being used. Example:
+
+    fn main() {
+        let x: i32 = 0;
+        let y = x; // ok!
+    }
+ */
+
+ /*
+
+    For the same reasons as above, conditional initialization is also not allowed
+    since there is a case that when if condition is false then "enigma" will remain uninitialized.
+
+  let enigma: i32;
+    if true {
+        enigma =1;
+    }
+    println!("{}",enigma);
+
+  */
+   
+}
