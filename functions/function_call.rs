e518f825f34e562705232f422f50ead137ca1995
ajitsoni007 <ajit.soni@amagi.com>
2024-01-22 23:59:30 +0530

    tailwind expression

--- functions/function_call.rs
+++ functions/function_call.rs
@@ -2,10 +2,18 @@ fn main(){
 
    let a=do_stuff(2.1,4.2);
    println!("{}",a);
+
+   let b=do_stuff_2(2.1,4.2);
+   println!("{}",b);
 }
 
 fn do_stuff(qty:f64, oz :f64) -> f64{
 
     // returning product of qty and oz
     return qty*oz;
+}
+
+fn do_stuff_2(qty:f64, oz :f64) -> f64{
+    // returning product of qty and oz using tailwing expression
+    qty*oz
 }
