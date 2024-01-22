06fd0246e2caaed2ca828022fdb20cac4b8daa17
ajitsoni007 <ajit.soni@amagi.com>
2024-01-22 23:47:22 +0530

    modified folder structure

---
+++ Exercises/Excercise A/variables/target/debug/deps/variables-35daf6d75a85b248.d
@@ -0,0 +1,5 @@
+/Users/ajitsoni/Desktop/rust_learning/Excercise A/variables/target/debug/deps/variables-35daf6d75a85b248: src/main.rs
+
+/Users/ajitsoni/Desktop/rust_learning/Excercise A/variables/target/debug/deps/variables-35daf6d75a85b248.d: src/main.rs
+
+src/main.rs:
