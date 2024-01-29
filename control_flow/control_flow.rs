539edb5afdef226862cee720bdc3ac4e759f0c07
ajit <ajitsoni000005@gmail.com>
2024-01-29 23:52:54 +0530

    conditional statements

---
+++ control_flow/control_flow.rs
@@ -0,0 +1,14 @@
+fn main(){
+    let mut num=2;
+    let mut msg="";
+
+    if num==2{
+        msg="two";
+    }else if num==3{
+        msg="three";
+    }else{
+        msg="other";
+    }
+    println!("{}",msg);
+    
+}
