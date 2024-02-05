890ecdcf59e6147e76ef2e3704784f0138d15c03
ajit <ajitsoni000005@gmail.com>
2024-02-05 22:23:50 +0530

    loops in rust

---
+++ loops/loop.rs
@@ -0,0 +1,19 @@
+fn main(){
+    
+    'bob: loop {
+        loop {
+            loop {
+                break 'bob;
+            }
+        }
+    }
+    
+    // while loops 
+
+    while dizzy(){
+        println!("dizzy");
+    }
+}
+fn dizzy()->bool{
+    return false;
+}
